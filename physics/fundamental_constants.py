c595d4522e3ceada8ff56d83e3fccc3c80a4977c
cjmcclellan <cmcclellan1993@gmail.com>
2020-10-22 12:40:03 -0700

    adjusting value loading

--- physics/fundamental_constants.py
+++ physics/fundamental_constants.py
@@ -4,5 +4,5 @@ from physics.value import ureg, Value
 free_space_permittivity_F_div_m = Value(value=8.85e-12, unit=ureg.farad/ureg.meter)
 free_space_permittivity_F_div_cm = Value(value=8.85e-14, unit=ureg.farad/ureg.centimeter)
 
-electron_charge_C = Value(value=1e-19, unit=ureg.coulomb)
+electron_charge_C = Value(value=1.602176634e-19, unit=ureg.coulomb)
 
